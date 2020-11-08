088b28322aa0d90b422c8be2587a101c96aa3144
Sergey Minakov <naithar@icloud.com>
2020-11-08 20:19:21 +0300

    admob test

---
+++ modules/admob/ios/src/AdmobBanner.h
@@ -0,0 +1,28 @@
+#import <GoogleMobileAds/GADBannerView.h>
+#import "app_delegate.h"
+
+#define BANNER_ENABLE_DELAY 5
+ 
+@class ViewController;
+
+@interface AdmobBanner: NSObject <GADBannerViewDelegate> {
+    GADBannerView *bannerView;
+    bool initialized;
+    bool isReal;
+    bool isOnTop;
+    int instanceId;
+    NSString *adUnitId;
+    ViewController *rootController;
+}
+
+- (void)initialize:(BOOL)is_real: (int)instance_id;
+- (void)loadBanner:(NSString*)bannerId :(BOOL)is_on_top;
+- (void)showBanner;
+- (void)hideBanner;
+- (void)disableBanner;
+- (void)enableBanner;
+- (void)resize;
+- (int)getBannerWidth;
+- (int)getBannerHeight;
+
+@end
